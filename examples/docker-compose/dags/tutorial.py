50f612c260af63027795037667a59d27dcb231b5
zav shotan <zav.shotan@h1insights.com>
2020-03-05 17:38:22 -0500

    Added tutorial 2

--- examples/docker-compose/dags/tutorial.py
+++ examples/docker-compose/dags/tutorial.py
@@ -31,7 +31,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 dag = DAG(
-    'tutorial-test-starter',
+    'tutorial',
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
