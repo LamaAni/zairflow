1181161816df695df4e24f11f9ef0d0efb99cd9f
Zav <zav@cherre.com>
2021-02-02 16:19:17 -0500

    Added network scripts

--- scripts/common.sh
+++ scripts/common.sh
@@ -8,3 +8,12 @@ source zbash_commons
 if [ $? -ne 0 ]; then
   echo "zbash_commons not found. Please see: https://github.com/LamaAni/zbash-commons"
 fi
+
+function load_scripts() {
+  for file in $SCRIPTS_PATH/common/*; do
+    source "$file"
+    assert $? "Error while loading lib file: $file" || return $?
+  done
+}
+
+load_scripts || exist $?
