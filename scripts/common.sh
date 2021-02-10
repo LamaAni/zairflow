96c0af854c8beaaee5be197f7e96c6b0898226a9
Zav <zav.shotan@gmail.com>
2021-02-10 16:49:32 -0500

    Added common changes

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+type zbash_commons &>/dev/null
+if [ $? -ne 0 ]; then
+  echo "[DOWNLOAD] Downloading zbash_commons from latest release.."
+  ZBASH_COMMONS_GET_SCRIPT="$(curl -Ls "https://raw.githubusercontent.com/LamaAni/zbash-commons/master/get?ts_$(date +%s)=$RANDOM")"
+  ZBASH_COMMONS="$(bash -c "$ZBASH_COMMONS_GET_SCRIPT")"
+  eval "$ZBASH_COMMONS"
+else
+  ZBASH_COMMONS="$(cat "$(which zbash_commons)")"
+  source zbash_commons
+fi
+
 SCRIPTS_PATH="$SCRIPTS_PATH"
 : ${SCRIPTS_PATH:="$(realpath $(dirname $(realpath "$BASH_SOURCE[0]")))"}
 : ${ZAIRFLOW_RUN_INITDB:="false"}
-
-source zbash_commons
-if [ $? -ne 0 ]; then
-  echo "zbash_commons not found. Please see: https://github.com/LamaAni/zbash-commons"
-fi
 
 function load_scripts() {
   for file in $SCRIPTS_PATH/common/*; do
